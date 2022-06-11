1cdaeed1dcaad8a9315bc7001edfdb7241f741a4
littlephillips <phillipkinuthia72@gmail.com>
2022-06-11 23:04:45 +0300

    modified index.js file

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+
+
+function findMatching (drivers, list){
+    return drivers.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === list.toLowerCase()
+    );
+    
+}
+
+
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(
+        (possibleMatch) => possibleMatch.toLowerCase().indexOf(letters.toLowerCase()) === 0
+    );
+
+}
+
+
+function matchName( drivers, listLetters) {
+    return drivers.filter((record) => record.name === listLetters);
+
+}
+
+
